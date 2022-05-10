7aaaea2ffd24a0dd37b54e7d983ed35eefe70cbf
Ubuntu <ubuntu@ip-172-31-26-177.us-west-2.compute.internal>
2022-05-10 07:02:47 +0000

    updaate

--- airflow/setup.sh
+++ airflow/setup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-pip3 install --upgrade pip
-sudo apt install libffi-dev
-pip3 install setuptools-rust
-pip3 install "apache-airflow[celery]==2.3.0" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.3.0/constraints-3.7.txt"
 
-pip3 boto3
+install_airflow=false
+start_local_airflow=false
+
+if [ "$install_airflow" = true ]
+then
+    pip3 install --upgrade pip
+    sudo apt install libffi-dev
+    pip3 install setuptools-rust
+    pip3 install "apache-airflow[celery]==2.3.0" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.3.0/constraints-3.7.txt"
+    pip3 boto3
+fi
+
+# https://airflow.apache.org/docs/apache-airflow/stable/start/local.html
+if [ "$start_local_airflow" = true ]
+then
+    airflow standalone
+fi
+
 
 aws s3 cp test.py s3://charnn-data/airflow/test.py
